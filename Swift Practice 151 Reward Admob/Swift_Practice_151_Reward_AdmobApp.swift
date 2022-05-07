0af60509285a710fe32f01ab133f80ee93477b7f
Dogpa <dogpa0130@gmail.com>
2022-05-07 14:37:42 +0800

    Initial Commit

---
+++ Swift Practice 151 Reward Admob/Swift_Practice_151_Reward_AdmobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_151_Reward_AdmobApp.swift
+//  Swift Practice 151 Reward Admob
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/7.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_151_Reward_AdmobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
